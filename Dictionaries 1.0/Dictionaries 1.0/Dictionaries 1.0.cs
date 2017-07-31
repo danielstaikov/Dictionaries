4d5ea132c0ba10c52dbff34580eb885252b4ad71
Dani0000 <daniel.staikov@gmail.com>
2017-07-31 21:16:00 +0300

    upload

---
+++ Dictionaries 1.0/Dictionaries 1.0/Dictionaries 1.0.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries_1._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phonebook = new Dictionary<string, string>();
+            phonebook["Pesho"] = "+15258623326";// добавя стойности в речника
+            phonebook["Gosho"] = "+1312232154684";
+            phonebook.Add("Maria", "+999666999");// друг начин за добавяне на стойности в речника
+            bool alalabala = phonebook.ContainsKey("Gosho"); // phonebook.ContainsKey() връща булева стойност
+
+            //Console.WriteLine(phonebook["Maria"]);
+
+            foreach (KeyValuePair<string, string> item in phonebook)
+            {
+                Console.WriteLine($"{item}");
+            }
+
+        }
+    }
+}
---
+++ Dictionaries 1.0/Dictionaries 2.0 Sorted/Dictionaries 2.0.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries_2._0_Sorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<string, string> phonebook = new SortedDictionary<string, string>();
+            phonebook["Pesho"] = "+15258623326";// добавя стойности в речника
+            phonebook["Gosho"] = "+1312232154684";
+            phonebook.Add("Maria", "+999666999");// друг начин за добавяне на стойности в речника
+            bool alalabala = phonebook.ContainsKey("Gosho"); // phonebook.ContainsKey() връща булева стойност
+
+            //Console.WriteLine(phonebook["Maria"]);
+
+            foreach (KeyValuePair<string, string> item in phonebook)
+            {
+                Console.WriteLine($"{item}");
+            }
+        }
+    }
+}
