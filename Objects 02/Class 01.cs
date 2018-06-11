1b92359817511cb1fc76c0fff863b429c127333e
Dani0000 <daniel.staikov@gmail.com>
2018-06-11 19:49:44 +0300

    Correct input parameters.

---
+++ Objects 02/Class 01.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objects_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero Batman = new Hero
+            {
+                SecretName = "Pesho",
+                HeroName = "Batman",
+                HeroStrength = 26
+
+            };
+
+            Hero Terminator = new Hero
+            {
+                SecretName = "Arnold Schwarzengger ",
+                HeroName = "Terminator",
+                HeroStrength = 148
+            };
+            List<Hero> heroes = new List<Hero>();
+            heroes.Add(Batman);
+            heroes.Add(Terminator);
+            foreach (Hero hero in heroes)
+            {
+                Console.WriteLine(hero.HeroName + " = " + hero.HeroStrength);
+            }
+        }
+    }
+    class Hero
+    {
+        public string SecretName { get; set; }
+        public string HeroName { get; set; }
+        public int HeroStrength { get; set; }
+    }
+}
---
+++ TraningLab20/TraningLab20/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraningLab20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+
+            double l = Math.Floor(length / 1.2);
+            double w = Math.Floor((width - 1) / 0.7);
+
+            double result = (l * w) - 3;
+            Console.WriteLine(result);
+        }
+    }
+}
